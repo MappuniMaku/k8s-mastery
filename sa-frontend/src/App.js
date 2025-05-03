9e6acbe45f6b37461ff61bd8627f2125496f342b
Grigory Telenok <telenok.grigory@gmail.com>
2025-05-03 15:59:25 +0300

    Edit Dockerfiles to include build steps

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch(process.env.BACKEND_URL, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
